e1b242913ca3ae35d409b34d05018e976d862ff3
tom <tom@sapletta.com>
2021-06-11 20:27:13 +0200

    update

---
+++ let_xml.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * @param $url
+ * @param null $callback
+ * @param false $associative
+ * @return false|mixed|string
+ *
+ * @throws Exception
+ */
+function let_xml($url, $callback = null, $associative = false)
+{
+    if (empty($url)){
+        throw new Exception("Url: " . $url . " is empty");
+    }
+
+    if (!file_exists($url)) {
+        throw new Exception("Url: " . $url . " not exist");
+    }
+    $file = file_get_contents($url, true);
+
+    if (is_callable($callback)) {
+        return $callback($file);
+    }
+
+    return $file;
+}
+
+
+/**
+ * Class LetJson
+ */
+class LetXml
+{
+    /** @var array|mixed */
+    public $xml = [];
+
+    /** @var string  */
+    public $url = '';
+
+    /**
+     * LetJson constructor.
+     * @param $url
+     */
+    function __construct($url)
+    {
+        $this->url = $url;
+        $this->xml = let_xml($url);
+    }
+
+    /**
+     * @return mixed
+     */
+    function first()
+    {
+        return $this->xml[0];
+    }
+
+    /**
+     * @param $callback
+     */
+    function each($callback)
+    {
+        foreach ($this->xml as $item) {
+            $callback($item);
+        }
+    }
+}
