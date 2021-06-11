07f9bed8b6c54e36939fb9352c6f5624cd25fef6
tom <tom@sapletta.com>
2021-06-11 21:35:04 +0200

    make with arrays

--- let_xml.php
+++ let_xml.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * @param $url
+ * @param array|string $url
  * @param null $callback
  * @param false $associative
  * @return false|mixed|string
@@ -13,18 +13,22 @@ function let_xml($url, $callback = null, $associative = false)
     if (empty($url)){
         throw new Exception("Url: $url is empty");
     }
+//    var_dump($url);
 
     $urls = [];
     if (!is_array($url)){
         $urls[] = $url;
+    } else {
+        $urls = $url;
     }
 
     $file = '';
-    foreach($urls as $url){
-        if (!file_exists($url)) {
-            throw new Exception("Url: $url not exist");
+    foreach($urls as $url_item){
+        if (!file_exists($url_item)) {
+            throw new Exception("Url: $url_item not exist");
         }
-        $file .= file_get_contents($url, true);
+        $file .= file_get_contents($url_item, true);
+        echo $url_item;
     }
 
     if (is_callable($callback)) {
